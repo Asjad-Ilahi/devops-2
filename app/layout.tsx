73dc6c2d31c021c8e1afbbf9ce971ea78db22e2f
Wahab-x78 <116077215+Wahab-x78@users.noreply.github.com>
2025-04-29 17:32:45 +0500

    Backend added website name

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'Iu Bank',
+  title: 'Free International Banking',
   description: 'Created by Venhash',
   generator: 'v0.dev',
 }
